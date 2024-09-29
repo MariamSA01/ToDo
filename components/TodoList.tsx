99186435f315f7b6c84d925381862af2de1d6878
MariamSA01 <msa01505@gmail.com>
2024-09-29 23:40:21 +0300

    handling Get and Post CRUD operations

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, FlatList } from 'react-native';
 import { useSelector } from 'react-redux';
 import TodoItem from './TodoItem';
+import { fetchTodos } from '../util/https';
+
+
+type Todo = {
+    id: string;
+    title: string;
+    completed: boolean;
+  };
 
 const TodoList = () => {
-  const todos = useSelector((state: any) => state.todos.todos);
+//   const todos = useSelector((state: any) => state.todos.todos);
+  const [fetchedTodos, setFetchedTodos] = useState<Todo[]>([])
+
+  useEffect(()=>{
+    async function getTodo(){
+        const todos = await fetchTodos()
+        setFetchedTodos(todos)
+    }
+    getTodo()
+  },[])
 
   return (
     <View>
       <FlatList 
-        data={todos}
+        data={fetchedTodos}
         keyExtractor={(item) => item.id.toString()}
         renderItem={({ item }) => <TodoItem todo={item} />}
       />
