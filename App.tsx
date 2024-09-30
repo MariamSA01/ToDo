8dceb7d5b561972032e030354a88db9a1e709f5a
MariamSA01 <msa01505@gmail.com>
2024-09-30 18:01:47 +0300

    files rescheduling to enable adding navigation

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import store from './redux/store';
-import TodoForm from './components/TodoForm';
-import TodoList from './components/TodoList';
+import store from './src/redux/store';
+import TodoForm from './src/components/TodoForm';
+import TodoList from './src/components/TodoList';
 
 
 const App = () => {
