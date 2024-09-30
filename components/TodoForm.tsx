42df922ddf052086aec506f48b740062e19425c9
MariamSA01 <msa01505@gmail.com>
2024-09-30 17:17:24 +0300

    style changes

--- components/TodoForm.tsx
+++ components/TodoForm.tsx
@@ -28,30 +28,44 @@ const TodoForm = () => {
         placeholder="Add a new task" 
         value={title} 
         onChangeText={setTitle} 
+        style={style.input}
       />
       <Button title="Add Todo" onPress={handleAddTodo} />
     </View>
   );
 };
 
-
 const style = StyleSheet.create({
-    container:{
-        padding: 10,
-        marginBottom: 10,
-        marginTop:'20%',
-        marginHorizontal: 10,
-        backgroundColor: '#f2f2f2',
-        borderRadius: 5,
-        elevation: 1,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.8,
-        shadowRadius: 2,
-        justifyContent: 'space-between',
-        flexDirection: 'row',
-        alignItems: 'center',
+  container: {
+    padding: 10,
+    marginBottom: 10,
+    marginTop: '20%',
+    marginHorizontal: 10,
+    backgroundColor: '#F5F5F5',
+    borderRadius: 5,
+    elevation: 1,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  input: {
+    flex: 1,
+    marginRight: 10,
+    backgroundColor: '#98FF98',
+    padding: 8,
+    borderRadius: 5,
+    borderColor: '#000000',
+    borderWidth: 1,
+  },
+  button: {
+    backgroundColor: '#FFD700',
+    padding: 10,
+    borderRadius: 5,
+  },
+});
 
-    },
-})
 export default TodoForm;
