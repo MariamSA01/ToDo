0eaa52b2f165fa2443bd2effcf0b254b320fe288
MariamSA01 <msa01505@gmail.com>
2024-09-30 23:07:02 +0300

    adding navigation

--- index.js
+++ index.js
@@ -6,4 +6,9 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+import { Provider } from 'react-redux';
+import store from './src/redux/store';
+const AppProvider=()=> <Provider store={store}>
+<App/>
+</Provider>
+AppRegistry.registerComponent(appName, () => AppProvider);
